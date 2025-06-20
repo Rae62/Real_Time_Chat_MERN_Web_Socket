436c6db0257c2c99753754c148fa16af444ec921
Rae62 <ludovic.duhamel-62260@outlook.fr>
2025-06-20 10:40:07 +0200

    predeploy

--- Back/src/index.js
+++ Back/src/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+const path = require("path");
+
+const __DIRNAME = path.resolve();
 
 import cors from "cors";
 
@@ -15,12 +18,18 @@ dotenv.config();
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 
+app.use(express.static(path.join(__DIRNAME, "/Front/dist")));
+
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
   })
 );
+
+app.get(/(.*)/, (req, res) => {
+  res.sendFile(path.join(__DIRNAME, "Front", "dist", "index.html"));
+});
 
 const PORT = process.env.PORT;
 
